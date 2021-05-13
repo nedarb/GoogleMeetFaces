3f317dbf2025aed61ac025eaaf3aa837fb18f82d
Braden Hays <whays@salesforce.com>
2021-05-12 20:00:39 -0700

    Add debugging page

---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+/**
+ * defined in webpack
+ */
+declare const __IN_DEBUG__: boolean;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,17 @@
 const merge = require("webpack-merge");
+const webpack = require("webpack");
 const path = require("path");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map", 
+  devtool: "inline-source-map",
   entry: {
     debug: path.join(__dirname, "src/pages/debug/index.tsx"),
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __IN_DEBUG__: JSON.stringify(true),
+    }),
+  ],
 });
